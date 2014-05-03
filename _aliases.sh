e99263433af483cd3b45b87d1d05e9e7e97f39f1
Sumio Singu <sumio.singu@access-company.com>
2014-05-03 23:28:39 +0900

    Separate aliases to _aliases.sh

---
+++ _aliases.sh
@@ -0,0 +1,53 @@
+alias a='alias'
+
+a g='git'
+a rm='rm -rf'
+a l='ls'
+a ll='l -lh'
+a la='l -a'
+a lla='l -la'
+a .='cd ..'
+a ..='cd ../..'
+a ...='cd ../../..'
+a ....='cd ../../../..'
+a md='mkdir -p'
+a less='less -N'
+a emacs='emacs -nw'
+a df='df -h'
+a du0='du -h -d 0'
+a du1='du -h -d 1'
+a du2='du -h -d 2'
+
+function zipr() {
+  zip -vr $1.zip $1 -x "*.DS_Store"
+}
+
+################################################ rails
+a bi='bundle install --path vendor/bundle'
+a r='bundle exec rake'
+a rdc='r db:create'
+a rdm='r db:migrate'
+a rdr='r db:rollback'
+a rdd='r db:drop:all'
+a rds='r db:seed'
+a rdfl='r db:fixtures:load'
+
+function bert() {
+  if [ -z $1 ]; then
+    ber test
+  else
+    ber test:units TEST=$1
+  fi
+}
+
+a rgf='r gettext:find'
+a rgp='r gettext:pack'
+a rjw='r jobs:work'
+
+a rl='bundle exec rails'
+a rlc='rl console'
+a rls='rl server'
+a rld='rl dbconsole'
+
+a pgr='pg_restore --verbose --clean --no-acl --no-owner'
+################################################ rails
--- _bash_profile.sh
+++ _bash_profile.sh
@@ -4,6 +4,8 @@ if [ -f ~/.bash_profile.local ]; then
   source ~/.bash_profile.local
 fi
 
+source ~/.aliases.sh
+
 export PATH=~/bin:${PATH}
 export PATH=~/bin.private:${PATH}
 export PATH=/opt/local/bin:${PATH}
@@ -30,88 +32,3 @@ source ~/.git-prompt.bash
 PS1='\[\033[1;35m\]\t\[\033[1;36m\][\u@\h:\[\033[33m\]\w$(__git_ps1 " (%s)")\[\033[36m\]]\$\[\033[0m\] '
 export CLICOLOR=1
 export LSCOLORS=DxGxcxdxCxegedabagacad
-
-
-# Aliases
-alias ll="ls -lh"
-alias la="ls -a"
-alias lla="ls -la"
-alias df="df -h"
-alias rmrf="rm -rf"
-alias emacs="emacs -nw"
-alias l="less -N"
-alias du="du -h"
-alias du0="du -h -d 0"
-alias du1="du -h -d 1"
-alias du2="du -h -d 2"
-
-alias r="rails"
-alias be="bundle exec"
-alias bi="bundle install --path vendor/bundle"
-alias ber="be rake"
-alias berdc="ber db:create"
-alias berdm="ber db:migrate"
-alias berdr="ber db:rollback"
-alias berdd="ber db:drop:all"
-alias berds="ber db:seed"
-alias berdfl="ber db:fixtures:load"
-#alias bert="ber test"
-function bert() {
-  if [ -z $1 ]; then
-    ber test
-  else
-    ber test:units TEST=$1
-  fi
-}
-alias bergf="ber gettext:find"
-alias bergp="ber gettext:pack"
-alias berjw="ber jobs:work"
-
-alias berl="be rails"
-alias berlc="berl console"
-alias berls="berl server"
-
-alias g="git"
-alias gf="g fetch"
-alias gs="g status"
-alias gl="g log"
-alias glm="gl master"
-alias gls="gl stable"
-alias glolr="gl --oneline --left-right"
-alias gd="g diff"
-alias gdh="gd HEAD"
-alias gdh1="gd HEAD~1 HEAD"
-alias gdh2="gd HEAD~2 HEAD~1"
-alias gdh3="gd HEAD~3 HEAD~2"
-alias gb="g branch"
-alias gbd="g branch -D"
-alias gco="g checkout"
-alias gcob="gco -b"
-alias gcom="gco master"
-alias gcos="gco stable"
-alias ga="g add"
-alias gr="g rm"
-alias grs="g reset"
-alias grsh="g reset --hard"
-alias gc="g commit"
-alias gcm="g commit -m"
-alias gca="g commit --amend"
-alias gpl="g pull"
-alias gplf="gpl -f"
-alias gps="g push -u"
-alias gpsf="gps -f"
-alias grb="g rebase"
-alias grbm="grb master"
-alias grbi="grb -i"
-alias grbc="grb --continue"
-alias gm="g merge"
-alias gms="g merge --squash"
-alias gt="g tag"
-alias gcfd="g clean -fd"
-alias gcp="g cherry-pick"
-alias gmv="g mv"
-alias gsi="g submodule init"
-alias gsu="g submodule update"
-
-
-alias pgr="pg_restore --verbose --clean --no-acl --no-owner"
