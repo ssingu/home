96b0b4b307163e0f43f8b645fdb311ccdf2b59c3
Sumio Singu <sumio.singu@access-company.com>
2014-06-21 22:58:29 +0900

    Refactor install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+cd $(dirname ${BASH_SOURCE})
 
-############################ sub routines
-pae() {
+
+############################ functions
+function osx? {
+  [ `uname` = 'Darwin' ]
+}
+
+function execute() {
   echo "[EXECUTE] $@"
   eval "$@"
 }
 
-ignore_file?() {
+function ignore_file?() {
   ignore_files=(README.md install.sh)
   for ignore_file in ${ignore_files[@]}; do
     if [ ${ignore_file} = $(basename $1) ]; then return 0; fi
@@ -15,30 +21,23 @@ ignore_file?() {
   return 1
 }
 
-backup_and_remove() {
+function backup_and_remove() {
   if [ -L $1 ]; then
-    pae unlink $1
+    execute unlink $1
   elif [ -e $1 ]; then
-    pae mv $1 /tmp/$(basename $1).$(date +%Y%m%d)
+    execute mv $1 /tmp/$(basename $1).$(date +%Y%m%d)
   fi
 }
-############################ sub routines
 
-
-############################  main routine
-cd $(dirname ${BASH_SOURCE})
-
-for src in $(ls); do
-  if ignore_file? ${src} ; then continue; fi
-  dst=~/$(echo ${src} | sed -e "s/^_/./")
-  if [[ ${src} =~ ^_bash_profile.*\.sh$ ]] ; then dst=${dst/.sh/}; fi
-  backup_and_remove ${dst}
-  pae ln -s -f ${PWD}/${src} ${dst}
-done
-
-if ! $(which brew > /dev/null); then
-    pae 'ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"'
-fi
+function create_symlink_of_dotfiles {
+  for src in $(ls); do
+    if ignore_file? ${src} ; then continue; fi
+    dst=~/$(echo ${src} | sed -e "s/^_/./")
+    if [[ ${src} =~ ^_bash_profile.*\.sh$ ]] ; then dst=${dst/.sh/}; fi
+    backup_and_remove ${dst}
+    execute ln -s -f ${PWD}/${src} ${dst}
+  done
+}
 
 BREW_PACKAGES="\
 cmigemo \
@@ -56,51 +55,69 @@ docker \
 node \
 git \
 "
+function install_packages_with_brew {
+  if ! $(which brew > /dev/null); then
+    execute 'ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"'
+  fi
 
-brew tap sanemat/font
-pae brew install ${BREW_PACKAGES}
+  brew tap sanemat/font
+  execute brew install ${BREW_PACKAGES}
 
-if [ ! -e /Applications/Emacs.app ]; then
-    pae brew install emacs --cocoa
-    pae ln -s $(find /usr/local/Cellar/emacs/ -name Emacs.app) /Applications
-fi
+  if [ ! -e /Applications/Emacs.app ]; then
+    execute brew install emacs --cocoa
+    execute ln -s $(find /usr/local/Cellar/emacs/ -name Emacs.app) /Applications
+  fi
 
-if ! $(ls ~/Library/Fonts/Ricty* > /dev/null); then
-  pae cp -f /usr/local/Cellar/ricty/*/share/fonts/Ricty*.ttf ~/Library/Fonts/
-  pae fc-cache -vf
-fi
+  if ! $(ls ~/Library/Fonts/Ricty* > /dev/null); then
+    execute cp -f /usr/local/Cellar/ricty/*/share/fonts/Ricty*.ttf ~/Library/Fonts/
+    execute fc-cache -vf
+  fi
 
-if $(rbenv version | grep system > /dev/null); then
-  ruby_version=1.9.3-p448
-  pae rbenv install ${ruby_version}
-  pae rbenv global ${ruby_version}
-  pae rbenv rehash
-fi
+  if $(rbenv version | grep system > /dev/null); then
+    ruby_version=1.9.3-p448
+    execute rbenv install ${ruby_version}
+    execute rbenv global ${ruby_version}
+    execute rbenv rehash
+  fi
+
+  if ! $(ls ~/Library/LaunchAgents/ | grep postgresql > /dev/null); then
+    execute ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+    execute launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+    execute createuser -d postgres
+  fi
+}
 
 GEMS="\
 bundler
 spork
 spork-testunit
 "
-pae gem install ${GEMS}
-pae rbenv rehash
+function install_gems {
+  execute gem install ${GEMS}
+  if osx?; then
+    execute rbenv rehash
+  fi
+}
 
-if ! $(ls ~/Library/LaunchAgents/ | grep postgresql > /dev/null); then
-  pae ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-  pae launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-  pae createuser -d postgres
-fi
-############################  main routine
-
-
-############################ notify additional operation
 todo() {
   echo "[TODO] $1"
 }
 
-echo ''
+function notify_additional_operation {
+  echo ''
+  if ! $(crontab -l | grep make-filelist > /dev/null); then
+    todo "Type 'crontb -e' and add job '0 1 * * * ruby ~/.emacs.d/make-filelist.rb > /tmp/myfiles.filelist' with 'MAILTO=\"\"'. myfiles.filelist is used by anything.el"
+  fi
+}
+############################ functions
 
-if ! $(crontab -l | grep make-filelist > /dev/null); then
-  todo "Type 'crontb -e' and add job '0 1 * * * ruby ~/.emacs.d/make-filelist.rb > /tmp/myfiles.filelist' with 'MAILTO=\"\"'. myfiles.filelist is used by anything.el"
+
+
+############################ main routine
+create_symlink_of_dotfiles
+if osx?; then
+  install_packages_with_brew
 fi
-############################ notify additional operation
+install_gems
+notify_additional_operation
+############################ main routine
